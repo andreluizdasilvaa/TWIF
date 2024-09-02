45f6da49aa3ba0b628319538d3c19f7b79505d0b
André Luiz <sandreluiz486@gmail.com>
2024-09-02 16:25:32 -0300

    Teste no pc do IF
    Adicionado um input nome ao cadastro teste( html ).

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -73,5 +73,5 @@ app.use(session({
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
